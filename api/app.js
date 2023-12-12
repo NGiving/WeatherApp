0af9032682bb6948d917944ea18ab1e5d4451e23
Giving <44300487+NGiving@users.noreply.github.com>
2023-12-12 15:23:06 -0500

    minor changes

--- api/app.js
+++ api/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
-require('dotenv').config();
 const app = express();
+require('dotenv').config();
+
 app.enable('trust proxy')
 app.use(express.static('public'));
 app.use(cookieParser());
@@ -17,13 +16,13 @@ app.use((cors(
   }
 )))
 
-// mongoose.set("strictQuery", false);
-// const mongoDB = process.env.NODE_ENV === 'production' ? process.env.DB_STRING_PROD : process.env.DB_STRING;
-// main().catch((err) => console.log(err));
-// mongoose.connection.once('open', () => { console.log('Connected to MongoDB') });
-// async function main() {
-//     await mongoose.connect(mongoDB);
-// };
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.NODE_ENV === 'production' ? process.env.DB_STRING_PROD : process.env.DB_STRING;
+main().catch((err) => console.log(err));
+mongoose.connection.once('open', () => { console.log('Connected to MongoDB') });
+async function main() {
+    await mongoose.connect(mongoDB);
+};
 
 // const routes = require('./routes/routes');
 // app.use('/api', routes);
