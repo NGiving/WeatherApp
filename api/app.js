cd3cae079ff4406be51f74302e5be5f69eca127c
Giving <44300487+NGiving@users.noreply.github.com>
2023-12-12 14:39:52 -0500

    change folder structure

--- api/app.js
+++ api/app.js
@@ -2,25 +2,28 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 const app = express();
 app.enable('trust proxy')
 app.use(express.static('public'));
 app.use(cookieParser());
+app.use((cors(
+  {
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+)))
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
-// mongoose.set("strictQuery", false);
-// const mongoDB = process.env.NODE_ENV === 'production' ? process.env.DB_STRING_PROD : process.env.DB_STRING;
-// main().catch((err) => console.log(err));
-// mongoose.connection.once('open', () => { console.log('Connected to MongoDB') });
-// async function main() {
-//     await mongoose.connect(mongoDB);
-// };
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.NODE_ENV === 'production' ? process.env.DB_STRING_PROD : process.env.DB_STRING;
+main().catch((err) => console.log(err));
+mongoose.connection.once('open', () => { console.log('Connected to MongoDB') });
+async function main() {
+    await mongoose.connect(mongoDB);
+};
 
 // const routes = require('./routes/routes');
 // app.use('/api', routes);
